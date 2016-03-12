77bf12b031a3132cbfa424b3535655b1ff851e35
afraz.confiz <afraz.delphi@gmail.com>
2016-03-12 19:48:37 +0500

    added option to input and load url

--- iOS-WKWebView/ViewController.swift
+++ iOS-WKWebView/ViewController.swift
@@ -15,27 +15,33 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     var webView: UIWebView!
     
+    @IBOutlet weak var reloadButton: UIButton?
+    @IBOutlet weak var textField: UITextField?
+    @IBOutlet weak var containerView: UIView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let path = NSBundle.mainBundle().pathForResource("Info", ofType: "plist") {
             let myDict = NSDictionary(contentsOfFile: path)
             let strURL = myDict?.objectForKey("url")
-            let useWK = myDict?.objectForKey("use_wk")
+            textField!.text = strURL as? String;
             
-            let url = NSURL(string: strURL as! String)
+            self.reloadButtonTaped(reloadButton!);
             
-            if useWK as! Bool
-            {
-                wkWebView.loadRequest(NSURLRequest(URL: url!))
-                wkWebView.allowsBackForwardNavigationGestures = true
-            }
-            else
-            {
-                webView.loadRequest(NSURLRequest(URL: url!))
-            }
+            containerView?.frame = CGRectMake(0, 33, (containerView?.frame.size.width)!, (containerView?.frame.size.height)!)
             
-            
+//            let url = NSURL(string: strURL as! String)
+//            
+//            if useWK as! Bool
+//            {
+//                wkWebView.loadRequest(NSURLRequest(URL: url!))
+//                wkWebView.allowsBackForwardNavigationGestures = true
+//            }
+//            else
+//            {
+//                webView.loadRequest(NSURLRequest(URL: url!))
+//            }
         }
         
         
@@ -66,7 +72,37 @@ class ViewController: UIViewController, WKNavigationDelegate {
             }
         }
     }
-
-
+    
+    @IBAction func reloadButtonTaped(sender: UIButton)
+    {
+        let strURL = textField?.text
+        
+        if let path = NSBundle.mainBundle().pathForResource("Info", ofType: "plist") {
+            let myDict = NSDictionary(contentsOfFile: path)
+            let useWK = myDict?.objectForKey("use_wk")
+            
+            let url = NSURL(string: strURL!)
+            
+            if useWK as! Bool
+            {
+                if ((containerView?.superview) != nil)
+                {
+                    containerView?.removeFromSuperview()
+                }
+                wkWebView.addSubview(containerView!)
+                wkWebView.loadRequest(NSURLRequest(URL: url!))
+                wkWebView.allowsBackForwardNavigationGestures = true
+            }
+            else
+            {
+                if ((containerView?.superview) != nil)
+                {
+                    containerView?.removeFromSuperview()
+                }
+                webView.addSubview(containerView!)
+                webView.loadRequest(NSURLRequest(URL: url!))
+            }
+        }
+    }
 }
 
